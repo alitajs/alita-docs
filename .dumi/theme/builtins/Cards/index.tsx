8abb5ddefcff34f8ad53db8fb09244947cf9dfd0
xiaohuoni <448627663@qq.com>
2021-04-07 09:50:03 +0800

    chore: 单独维护主题

--- .dumi/theme/builtins/Cards/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react';
-import './index.less';
-
-const Cards: FC = ({ children }) => (
-  <div className="dumi-alita-cards">{children}</div>
-);
-
-export default Cards;
--- .dumi/theme/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Button } from './builtins/Button';
-export { default as Card } from './builtins/Card';
-export { default as Cards } from './builtins/Cards';
-export { default as ColorAccordion } from './builtins/ColorAccordion';
-export { default as CodeColor } from './builtins/CodeColor';
-export { default as ContributorList } from './builtins/ContributorList';
-export { default as Item } from './builtins/Item';
