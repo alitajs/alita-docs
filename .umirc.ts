82521a21cfa9fb6bd8c7f58b8b4d6f8f5ff5dc18
xiaohuoni <448627663@qq.com>
2021-11-17 17:46:15 +0800

    chore: add publish name

--- .umirc.ts
+++ .umirc.ts
@@ -15,6 +15,8 @@ export default defineConfig({
   ghPages: {
     branch: 'gh-pages',
     silent: true,
+    name: 'xiaohuoni',
+    email: '448627663@qq.com',
     repo: `https://${process.env.GH_TOKEN}@github.com/alitajs/alita-docs.git`,
   },
   mode: 'site',
